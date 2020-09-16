c62e80631fe8d3dccb17f486c90f1578d2031918
Joaquin Polonuer <jtpolonuer@gmail.com>
2020-09-16 16:13:16 -0300

    a

---
+++ bot.py
@@ -0,0 +1,11 @@
+from instapy import InstaPy
+
+session = InstaPy(username="yourprints3d", password="megapolo10")
+session.login()
+a = session.like_by_tags(["3dprint", "impresion3d"], amount=5)
+print(a)
+session.set_dont_like(["naked", "nsfw"])
+session.set_do_follow(True, percentage=50)
+session.set_do_comment(True, percentage=50)
+session.set_comments(["Nice!", "Sweet!", "Beautiful :heart_eyes:"])
+session.end()
