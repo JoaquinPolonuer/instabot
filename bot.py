aa1cfaefad768f6f4e2fb51fe8016d5e09962aca
Joaquin Polonuer <jtpolonuer@gmail.com>
2020-09-16 16:21:23 -0300

    set comment criteria

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ from instapy import InstaPy
 
 session = InstaPy(username="", password="")
 session.login()
-a = session.like_by_tags(["3dprint", "impresion3d"], amount=5)
-print(a)
+
 session.set_dont_like(["naked", "nsfw"])
 session.set_do_follow(True, percentage=50)
 session.set_do_comment(True, percentage=50)
-session.set_comments(["Nice!", "Sweet!", "Beautiful :heart_eyes:"])
+session.set_comments(
+    ["Buenisimo!", "Me encantaaa!", "Que buenooo :heart_eyes:"])
+session.like_by_tags(["3dprint", "impresion3d"], amount=5)
+
 session.end()
