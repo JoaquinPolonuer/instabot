88d6df034f99fadf490e1dbba88ae413da701c25
joacopolo <jtpolonuer@gmail.com>
2020-09-21 10:32:45 -0300

    finished version

--- bot.py
+++ bot.py
@@ -1,15 +1,25 @@
 
 from instapy import InstaPy
 from instapy import set_workspace
+from sys import platform
 import os
+import time
 
 # Setea la carpeta donde guardaremos los registros y la info
 set_workspace(os.getcwd())
 
 # crea una sesion
 # headless = True es sin abrir el navegador
+user = input("ingresar usuario: ")
+passwd = input("ingresar contraseña: ")
+etiquetas = input("ingresar hashtags separados por un espacio: ").split()
+accounts = input("ingresar cuentas similares separadas por un espacio:").split()
+comentarios = input("ingresar comentarios separados por guiones bajos: ").split("_")
 
-session = InstaPy(username="yourprints3d", password="megapolo10",headless_browser=True, geckodriver_path='./geckodriver')
+if platform == "linux" or platform=="linux2":
+    session = InstaPy(username=user, password=passwd,headless_browser=True, geckodriver_path='./geckodriver')
+else:
+    session = InstaPy(username=user, password=passwd,headless_browser=True)
 
 # se loguea a instagram
 session.login()
@@ -21,8 +31,7 @@ session.set_do_comment(True, percentage=50)
 session.set_quota_supervisor(
     enabled=True, peak_comments_daily=15, peak_comments_hourly=2, peak_follows_daily=70, peak_follows_hourly=4, peak_likes_daily=300, peak_likes_hourly=20)
 
-session.set_comments(
-    ["Buenisimo!", "Me encantaaa!", "Que buenooo :heart_eyes:", "Epico"])
+session.set_comments(comentarios)
 session.set_relationship_bounds(enabled=True,
                                 delimit_by_numbers=True,
                                 max_followers=5000,
@@ -31,14 +40,17 @@ session.set_relationship_bounds(enabled=True,
 
 
 # comenzamos a usar el bot
-session.like_by_feed(amount=10, randomize=True, unfollow=False, interact=False)
-session.like_by_tags(["3dprint", "impresion3d"], amount=50)
-session.follow_user_followers(['3ntre.detalles', 'instan3d', 'crazynozzlesba'],
-                              randomize=False, interact=True)
+for i in range(3):
+    session.like_by_feed(amount=10, randomize=True, unfollow=False, interact=False)
+    session.like_by_tags(etiquetas, amount=50)
+    session.follow_user_followers(accounts,
+                                randomize=False, interact=True)
 
-""" First step of Unfollow action - Unfollow not follower users..."""
-session.unfollow_users(amount=500, InstapyFollowed=(True, "nonfollowers"),
-                       style="FIFO",
-                       unfollow_after=60, sleep_delay=601)
-#    unfollow_after=12 * 60 * 60, sleep_delay=601)
+    """ First step of Unfollow action - Unfollow not follower users..."""
+
+    session.unfollow_users(amount=500, instapy_followed_enabled=True, 
+                            instapy_followed_param="nonfollowers",
+                            style="FIFO",
+                            unfollow_after=1, sleep_delay=601)
+    time.sleep(60)
 session.end()
